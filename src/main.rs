8e76504d244eedf4a92e48e3801446628eae4490
Peter Bartyik <peter@solsys.io>
2020-07-27 16:10:10 +0200

    taking shape

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,42 @@
 use regex::Regex;
 use rexpect::process;
 use rexpect::spawn;
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
 use std::process::Command;
-use std::{thread, time};
+use std::time;
+use tokio::prelude::*;
+use tokio::sync::watch;
+use tokio::task;
+use tokio::time::delay_for;
 
 // hooks layout-change window-add, session-create
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Prompt {
     name: String,
     regex: Regex,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Window {
     id: u32,
     panes: Vec<u32>,
+    pane_names: HashMap<u32, String>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Tree {
+    prompts: Vec<Prompt>,
     sessions: Vec<u32>,
     windows: Vec<Window>,
 }
 
 impl Tree {
-    fn new() -> Self {
+    fn new(prompts: Vec<Prompt>) -> Self {
         let mut tree = Self {
+            prompts: prompts,
             sessions: vec![],
             windows: vec![],
         };
@@ -120,6 +130,37 @@ impl Tree {
         }
         panic!("Pane id {} does not belong to any windows", pane_id);
     }
+    fn process_line(&self, line: &str) {
+        for prompt in &self.prompts {
+            match prompt.regex.captures(line) {
+                Some(cap) => {
+                    let pane_id = cap.get(1).unwrap().as_str().parse::<u32>().unwrap();
+
+                    let title = match prompt.name.as_str() {
+                        "titan" => {
+                            let filler = cap.get(2).unwrap().as_str();
+                            format!("{fill}@titan", fill = filler)
+                        }
+                        _ => format!("{}", prompt.name),
+                    };
+                    let window_id = self.clone().window_from_pane(pane_id);
+                    /*
+                    match Command::new("/usr/bin/tmux")
+                    .arg("rename-window")
+                    .arg("-t")
+                    .arg(display)
+                    .arg(title)
+                    .output()
+                    {
+                    Ok(_) => (),
+                    Err(e) => panic!(e),
+                    }
+                    */
+                }
+                None => (),
+            }
+        }
+    }
 }
 
 impl Window {
@@ -127,6 +168,7 @@ impl Window {
         return Window {
             id: window_id,
             panes: vec![],
+            pane_names: HashMap::new(),
         };
     }
 }
@@ -137,7 +179,14 @@ impl Window {
 // TODO add other hosts from config file
 // TODO exit/sleep once all tmux sessions terminate
 
-fn main() {
+enum Instruction {
+    Refresh,       // e.g. layout change
+    AttachTo(u32), // attach to session id u32
+    DoNothing,
+}
+
+#[tokio::main]
+async fn main() {
     // first get running sessions, windows and panes of tmux
     // tmux list-panes -a -F #{session_id}#{window_id}#{pane_id}
     //let re_sess = Regex::new(r"^(\d+):\s+.*").unwrap();
@@ -147,53 +196,107 @@ fn main() {
         regex: Regex::new(r"^%output\s+(%\d+)\s+([A-Za-z\d]+)@titan:~\$\s*$").unwrap(),
     }];
 
-    let mut tree = Tree::new();
-    panic!("asd");
-    //let mut session = sessions[0];
-    let session = "4";
-    // attach to session command mode
+    // 0. gather tree and spawn attaches to existing sessions
+    // 1. start listening for changes
+    // 2. change tree based on changes
+    let mut tree = Tree::new(prompts);
+    let (tx, rx) = watch::channel(tree.clone());
 
-    let delay = time::Duration::from_millis(10);
+    for session_id in tree.sessions.clone() {
+        let my_rx = rx.clone();
+        task::spawn(async move {
+            // attach to session command mode
 
-    let cmd = format!("/usr/bin/tmux -C attach -t {}", session);
-    // find a way to set timeout for unlimited
-    let mut sess = spawn(&cmd, Some(0)).unwrap();
-    while sess.process.status().unwrap() == process::wait::WaitStatus::StillAlive {
-        let r_line = sess.read_line();
-        match r_line {
-            Ok(x) => {
-                process_line(&x, &prompts);
+            let my_session_id = session_id.clone();
+            let cmd = format!("/usr/bin/tmux -C attach -t {}", my_session_id);
+            let mut sess = spawn(&cmd, Some(0)).unwrap();
+            let delay = time::Duration::from_millis(10);
+
+            while sess.process.status().unwrap() == process::wait::WaitStatus::StillAlive {
+                let r_line = sess.read_line();
+                match r_line {
+                    Ok(line) => {
+                        let current_tree = my_rx.borrow();
+                        current_tree.process_line(&line);
+                    }
+                    Err(_) => {
+                        delay_for(delay).await;
+                    }
+                };
             }
-            Err(_) => thread::sleep(delay),
-        };
+        });
+    }
+
+    let listener = bind("/tmp/tmux_renamer.sock");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => match process_input_from_tmux(stream) {
+                Instruction::Refresh => {
+                    println!("refresh tree");
+                    tree.refresh();
+                    tx.broadcast(tree.clone()).unwrap();
+                }
+                Instruction::AttachTo(session_id) => {
+                    println!("supposed to attach to window {}", session_id);
+                    let my_rx = rx.clone();
+                    task::spawn(async move {
+                        // attach to session command mode
+
+                        let my_session_id = session_id.clone();
+                        let cmd = format!("/usr/bin/tmux -C attach -t {}", my_session_id);
+                        let mut sess = spawn(&cmd, Some(0)).unwrap();
+                        let delay = time::Duration::from_millis(10);
+
+                        while sess.process.status().unwrap()
+                            == process::wait::WaitStatus::StillAlive
+                        {
+                            let r_line = sess.read_line();
+                            match r_line {
+                                Ok(line) => {
+                                    let current_tree = my_rx.borrow();
+                                    current_tree.process_line(&line);
+                                }
+                                Err(_) => {
+                                    delay_for(delay).await;
+                                }
+                            };
+                        }
+                    });
+                }
+                Instruction::DoNothing => (),
+            },
+            Err(e) => {
+                panic!("something went wrong here {}", e);
+            }
+        }
     }
 }
 
-fn process_line(line: &str, prompts: &Vec<Prompt>) {
-    for prompt in prompts {
-        match prompt.regex.captures(line) {
-            Some(cap) => {
-                let display = cap.get(1).unwrap().as_str();
-
-                let title = match prompt.name.as_str() {
-                    "titan" => {
-                        let filler = cap.get(2).unwrap().as_str();
-                        format!("{fill}@titan", fill = filler)
-                    }
-                    _ => format!("{}", prompt.name),
-                };
-                match Command::new("/usr/bin/tmux")
-                    .arg("rename-window")
-                    .arg("-t")
-                    .arg(display)
-                    .arg(title)
-                    .output()
-                {
-                    Ok(_) => (),
-                    Err(e) => panic!(e),
-                }
-            }
-            None => (),
-        }
+fn process_input_from_tmux(stream: std::os::unix::net::UnixStream) -> Instruction {
+    // layout change should trigger refresh
+    // session creation should trigger AttachTo
+    let mut reader = BufReader::new(stream);
+    let mut input = String::new();
+    reader.read_line(&mut input).unwrap();
+    println!("Got input: {:?}", input);
+    let re_layout_changed = Regex::new(r"^\s*layout\s+changed\s+").unwrap();
+    if re_layout_changed.is_match(&input) {
+        return Instruction::Refresh;
     }
+    let re_sess_createad = Regex::new(r"^\s*session\s+created\s+\$(\d+)\s*$").unwrap();
+    match re_sess_createad.captures(&input) {
+        Some(session_id) => {
+            let u32_sess = session_id.get(1).unwrap().as_str().parse::<u32>().unwrap();
+            return Instruction::AttachTo(u32_sess);
+        }
+        None => (),
+    }
+    Instruction::DoNothing
+}
+
+fn bind(path: impl AsRef<Path>) -> std::os::unix::net::UnixListener {
+    let path = path.as_ref();
+    // https://stackoverflow.com/questions/40218416/how-do-i-close-a-unix-socket-in-rust
+    std::fs::remove_file(path);
+    std::os::unix::net::UnixListener::bind(path).unwrap()
 }

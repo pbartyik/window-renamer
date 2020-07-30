0cfa55ae869b16e357167fd44c6434a4f2be8d2a
Peter Bartyik <peter@solsys.io>
2020-07-30 12:16:01 +0200

    somewhat works now

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,9 @@ use std::path::Path;
 use std::process::Command;
 use std::time;
 use tokio::prelude::*;
-use tokio::sync::watch;
+use tokio::sync::mpsc;
 use tokio::task;
 use tokio::time::delay_for;
-
-// hooks layout-change window-add, session-create
 
 #[derive(Debug, Clone)]
 struct Prompt {
@@ -20,25 +18,20 @@ struct Prompt {
 }
 
 #[derive(Debug, Clone)]
-struct Window {
-    id: u32,
-    panes: Vec<u32>,
-    pane_names: HashMap<u32, String>,
-}
-
-#[derive(Debug, Clone)]
 struct Tree {
     prompts: Vec<Prompt>,
     sessions: Vec<u32>,
-    windows: Vec<Window>,
+    windows: HashMap<u32, Vec<u32>>, // Window ID, [Pane ID,]
+    panes: HashMap<u32, String>,     // Pane ID, Pane title
 }
 
 impl Tree {
     fn new(prompts: Vec<Prompt>) -> Self {
         let mut tree = Self {
-            prompts: prompts,
+            prompts,
             sessions: vec![],
-            windows: vec![],
+            windows: HashMap::new(),
+            panes: HashMap::new(),
         };
 
         let output = Command::new("/usr/bin/tmux")
@@ -61,19 +54,33 @@ impl Tree {
                     let pane_id = cap.get(3).unwrap().as_str().parse::<u32>().unwrap();
 
                     tree.sessions.push(session_id);
-                    tree.push_window(window_id);
-                    tree.add_pane_to_window(window_id, pane_id);
+                    match tree.windows.get_mut(&window_id) {
+                        Some(window) => {
+                            window.push(pane_id);
+                        }
+                        None => {
+                            tree.windows.insert(window_id, vec![pane_id]);
+                        }
+                    }
+                    match tree.panes.get_mut(&pane_id) {
+                        Some(_) => {}
+                        None => {
+                            tree.panes.insert(pane_id, String::new());
+                        }
+                    }
                 }
                 None => continue,
             }
         }
         tree.sessions.dedup();
+        println!("Tree@init is {:?}", tree);
 
         tree
     }
     fn refresh(&mut self) {
+        println!("refresh called");
         self.sessions = vec![];
-        self.windows = vec![];
+        self.windows = HashMap::new();
         let output = Command::new("/usr/bin/tmux")
             .arg("list-panes")
             .arg("-a")
@@ -94,46 +101,103 @@ impl Tree {
                     let pane_id = cap.get(3).unwrap().as_str().parse::<u32>().unwrap();
 
                     self.sessions.push(session_id);
-                    self.push_window(window_id);
-                    self.add_pane_to_window(window_id, pane_id);
+                    match self.windows.get_mut(&window_id) {
+                        Some(window) => {
+                            window.push(pane_id);
+                        }
+                        None => {
+                            self.windows.insert(window_id, vec![pane_id]);
+                        }
+                    }
+                    match self.panes.get_mut(&pane_id) {
+                        Some(_) => {}
+                        None => {
+                            self.panes.insert(pane_id, String::new());
+                        }
+                    }
                 }
                 None => continue,
             }
         }
         self.sessions.dedup();
+        println!("after refresh {:?}", self);
     }
-    fn push_window(&mut self, window_id: u32) {
-        for window in &self.windows {
-            if window.id == window_id {
-                return;
-            }
-        }
-        let window = Window::new(window_id);
-        self.windows.push(window);
-    }
-    fn add_pane_to_window(&mut self, window_id: u32, pane_id: u32) {
-        for window in &mut self.windows {
-            if window.id == window_id {
-                window.panes.push(pane_id);
-                return;
-            }
-        }
-        panic!("No window {} for pane {}", window_id, pane_id);
-    }
-    fn window_from_pane(self, pane_id: u32) -> u32 {
-        for window in &self.windows {
-            for pane in &window.panes {
-                if pane == &pane_id {
-                    return window.id;
+    fn get_window_of_pane(self, pane_id: u32) -> Option<u32> {
+        for (w_id, p_ids) in &self.windows {
+            for p_id in p_ids {
+                if p_id == &pane_id {
+                    let window_id = *w_id;
+                    return Some(window_id);
                 }
             }
         }
-        panic!("Pane id {} does not belong to any windows", pane_id);
+        None
     }
-    fn process_line(&self, line: &str) {
+    fn update_window_title(&self, window_id: u32) {
+        let panes = self.windows.get(&window_id).unwrap();
+        let mut pane_count = HashMap::new();
+        let mut window_title = String::new();
+        // process existing names and come up with concatenated name
+        for pane in panes {
+            let pane_name = self.panes.get(&pane).unwrap();
+            match pane_count.get_mut(pane_name) {
+                Some(count) => {
+                    let new_count = *count + 1;
+                    pane_count.insert(pane_name, new_count);
+                }
+                None => {
+                    pane_count.insert(pane_name, 1);
+                }
+            }
+        }
+        for (pane_name, count) in &pane_count {
+            match count {
+                1 => {
+                    window_title.push_str(&pane_name);
+                }
+                _ => {
+                    let _title = format!("{} x {}", pane_name, count);
+                    window_title.push_str(&_title);
+                }
+            }
+        }
+        let window_name = format!("@{}", window_id);
+        match Command::new("/usr/bin/tmux")
+            .arg("rename-window")
+            .arg("-t")
+            .arg(window_name)
+            .arg(window_title)
+            .output()
+        {
+            Ok(_) => (),
+            Err(e) => panic!(e),
+        }
+    }
+    fn update_pane_title(self, pane_id: u32) {
+        println!("update_wt pane_id {}", pane_id);
+        let window_id = self.clone().get_window_of_pane(pane_id).unwrap();
+        /*
+        let mut window_id = 4294967295;
+        for (w_id, p_ids) in &self.windows {
+        for p_id in p_ids {
+        if p_id == &pane_id {
+        window_id = *w_id;
+        }
+        }
+        }
+        if window_id == 4294967295 {
+        println!("Tree {:?}", self);
+        panic!("no window found");
+        }
+        */
+
+        self.update_window_title(window_id.clone());
+    }
+    fn process_output(&mut self, line: &str) {
         for prompt in &self.prompts {
             match prompt.regex.captures(line) {
                 Some(cap) => {
+                    println!("cap '{:?}'", cap);
                     let pane_id = cap.get(1).unwrap().as_str().parse::<u32>().unwrap();
 
                     let title = match prompt.name.as_str() {
@@ -143,45 +207,43 @@ impl Tree {
                         }
                         _ => format!("{}", prompt.name),
                     };
-                    let window_id = self.clone().window_from_pane(pane_id);
-                    /*
-                    match Command::new("/usr/bin/tmux")
-                    .arg("rename-window")
-                    .arg("-t")
-                    .arg(display)
-                    .arg(title)
-                    .output()
-                    {
-                    Ok(_) => (),
-                    Err(e) => panic!(e),
-                    }
-                    */
+                    self.panes.insert(pane_id, title);
+                    self.clone().update_pane_title(pane_id);
                 }
                 None => (),
             }
         }
     }
-}
-
-impl Window {
-    fn new(window_id: u32) -> Self {
-        return Window {
-            id: window_id,
-            panes: vec![],
-            pane_names: HashMap::new(),
-        };
+    fn remove_pane(&mut self, pane_id: u32) {
+        self.panes.remove(&pane_id);
+        let window_id = self.clone().get_window_of_pane(pane_id).unwrap();
+        let index = self.windows[&window_id]
+            .iter()
+            .position(|pid| *pid == pane_id)
+            .unwrap();
+        let mut windows = self.windows.get_mut(&window_id).unwrap().clone();
+        windows.remove(index);
+        self.windows.insert(window_id, windows);
+        //self.windows[&window_id].remove(index); // this produced error
+        //self.windows[&window_id].remove_item(&pane_id); // this is unstable
+        self.update_window_title(window_id);
     }
 }
 
 // TODO tmux hook to start this upon session
-// TODO get target session via cli arg and attach to that, this way one instance will run per
-// session
 // TODO add other hosts from config file
 // TODO exit/sleep once all tmux sessions terminate
 
+enum TreeInstruction {
+    ProcessLine(String),
+    RemovePane(u32),
+    Refresh,
+}
+
 enum Instruction {
-    Refresh,       // e.g. layout change
-    AttachTo(u32), // attach to session id u32
+    Refresh,         // e.g. layout change
+    AttachTo(u32),   // attach to session id u32
+    RemovePane(u32), // remove pane
     DoNothing,
 }
 
@@ -193,20 +255,19 @@ async fn main() {
 
     let prompts = vec![Prompt {
         name: "titan".to_string(),
-        regex: Regex::new(r"^%output\s+(%\d+)\s+([A-Za-z\d]+)@titan:~\$\s*$").unwrap(),
+        regex: Regex::new(r"^%output\s+%(\d+)\s+([A-Za-z\d]+)@titan:~\$\s*$").unwrap(),
     }];
 
     // 0. gather tree and spawn attaches to existing sessions
     // 1. start listening for changes
     // 2. change tree based on changes
     let mut tree = Tree::new(prompts);
-    let (tx, rx) = watch::channel(tree.clone());
+    let (mut tx, mut rx) = mpsc::channel(100);
 
     for session_id in tree.sessions.clone() {
-        let my_rx = rx.clone();
-        task::spawn(async move {
-            // attach to session command mode
+        let mut my_tx = tx.clone();
 
+        task::spawn(async move {
             let my_session_id = session_id.clone();
             let cmd = format!("/usr/bin/tmux -C attach -t {}", my_session_id);
             let mut sess = spawn(&cmd, Some(0)).unwrap();
@@ -216,8 +277,7 @@ async fn main() {
                 let r_line = sess.read_line();
                 match r_line {
                     Ok(line) => {
-                        let current_tree = my_rx.borrow();
-                        current_tree.process_line(&line);
+                        my_tx.send(TreeInstruction::ProcessLine(line)).await;
                     }
                     Err(_) => {
                         delay_for(delay).await;
@@ -226,6 +286,21 @@ async fn main() {
             }
         });
     }
+    task::spawn(async move {
+        while let Some(msg) = rx.recv().await {
+            match msg {
+                TreeInstruction::ProcessLine(line) => {
+                    tree.process_output(&line);
+                }
+                TreeInstruction::Refresh => {
+                    tree.refresh();
+                }
+                TreeInstruction::RemovePane(pane_id) => {
+                    tree.remove_pane(pane_id);
+                }
+            }
+        }
+    });
 
     let listener = bind("/tmp/tmux_renamer.sock");
     for stream in listener.incoming() {
@@ -233,12 +308,14 @@ async fn main() {
             Ok(stream) => match process_input_from_tmux(stream) {
                 Instruction::Refresh => {
                     println!("refresh tree");
-                    tree.refresh();
-                    tx.broadcast(tree.clone()).unwrap();
+                    tx.send(TreeInstruction::Refresh).await;
+                }
+                Instruction::RemovePane(pane_id) => {
+                    tx.send(TreeInstruction::RemovePane(pane_id)).await;
                 }
                 Instruction::AttachTo(session_id) => {
                     println!("supposed to attach to window {}", session_id);
-                    let my_rx = rx.clone();
+                    let mut my_tx = tx.clone();
                     task::spawn(async move {
                         // attach to session command mode
 
@@ -253,8 +330,7 @@ async fn main() {
                             let r_line = sess.read_line();
                             match r_line {
                                 Ok(line) => {
-                                    let current_tree = my_rx.borrow();
-                                    current_tree.process_line(&line);
+                                    my_tx.send(TreeInstruction::ProcessLine(line)).await;
                                 }
                                 Err(_) => {
                                     delay_for(delay).await;
@@ -279,7 +355,7 @@ fn process_input_from_tmux(stream: std::os::unix::net::UnixStream) -> Instructio
     let mut input = String::new();
     reader.read_line(&mut input).unwrap();
     println!("Got input: {:?}", input);
-    let re_layout_changed = Regex::new(r"^\s*layout\s+changed\s+").unwrap();
+    let re_layout_changed = Regex::new(r"^\s*layout\s+changed\s*").unwrap();
     if re_layout_changed.is_match(&input) {
         return Instruction::Refresh;
     }
@@ -289,7 +365,15 @@ fn process_input_from_tmux(stream: std::os::unix::net::UnixStream) -> Instructio
             let u32_sess = session_id.get(1).unwrap().as_str().parse::<u32>().unwrap();
             return Instruction::AttachTo(u32_sess);
         }
-        None => (),
+        None => {}
+    }
+    let re_remove_pane = Regex::new(r"^\s*remove\s+pane\s+%?(\d+)\s*").unwrap();
+    match re_remove_pane.captures(&input) {
+        Some(pane_id) => {
+            let u32_pane_id = pane_id.get(1).unwrap().as_str().parse::<u32>().unwrap();
+            return Instruction::RemovePane(u32_pane_id);
+        }
+        None => {}
     }
     Instruction::DoNothing
 }
